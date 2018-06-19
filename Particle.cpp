bd006607b3039001aabfb0627fb91e6304911782
Niels Aka <6137753+nielsaka@users.noreply.github.com>
2018-06-19 21:43:29 +0200

    lesson 75: further blurring

--- Particle.cpp
+++ Particle.cpp
@@ -4,13 +4,24 @@
 
 namespace cop{
 Particle::Particle(): m_x(0), m_y(0) {
-	m_direction = (2.0 * M_PI * rand() / RAND_MAX);
-	m_speed = 0.0001 * rand() / RAND_MAX;
+	init();
 };
 Particle::~Particle() {}; // nothing happens??
 
+void Particle::init() {
+	// reset coordinates
+	m_x = 0;
+	m_y = 0;
+
+	m_direction = (2.0 * M_PI * rand() / RAND_MAX);
+	m_speed = 0.01 * rand() / RAND_MAX;
+	m_speed *= m_speed;
+}
+
 void Particle::update(int interval) {
 	// side-effects!!
+
+	m_direction += interval * 0.00008;
 
 	double xspeed = m_speed * cos(m_direction);
 	double yspeed = m_speed * sin(m_direction);
@@ -26,6 +37,19 @@ void Particle::update(int interval) {
 		m_direction += M_PI;
 	}
 	*/
+
+	// if off screen, reset to center
+	if (m_x <= -1.0 || m_x >= 1.0) {
+		init(); // call to this_particle_object.init() !! knows for which object to call init()
+	}
+	if (m_y <= -1.0 || m_y >= 1.0) {
+		init();	
+	}
+
+	//recenter at random
+	if (rand() < RAND_MAX / 100) {
+		init();
+	}
 }; 
 
 }
--- Particle.h
+++ Particle.h
@@ -3,19 +3,22 @@
 
 namespace cop {
 struct Particle {
+private:
+	double m_speed;
+	double m_direction;
 //private:
 //public: 
 // break encapsulation for sake of efficiency; no need to go via access method..
 // in games, simulations etc. need to make compromises
+public:
 	double m_x; // "m_" stands for member variable 
 	double m_y; // why double? be able to change gradually
 
-	double m_speed;
-	double m_direction;
 // alternatively, use "struct" instead of "class"
 // default in class: member objects are private
 // default in struct: member objects are public 
 public:
+	void init();
 	Particle();
 	~Particle();
 	void update(int interval);
